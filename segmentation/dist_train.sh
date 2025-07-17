1e2d34cfe603d83b1a607491339fb6daf20ad136
iacopo97 <iacopo.curti@studio.unibo.it>
2025-07-17 10:34:53 +0000

    eigth commit

--- segmentation/dist_train.sh
+++ segmentation/dist_train.sh
@@ -3,5 +3,5 @@ CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29301}
 
-CUDA_VISIBLE_DEVICES=0,1  python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+CUDA_VISIBLE_DEVICES=2,3  python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch --deterministic ${@:3} 
